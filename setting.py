e5ace69de07a9047286e2cf3a01e58cd875ac30b
Mazen <aounallah.mazen@gmail.com>
2019-12-22 14:47:20 +0100

    background and ship

---
+++ setting.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for alien invasion"""
+    def __init__(self):
+        """Initialize the game settings"""
+        # screen settings
+        self.screen_width = 800
+        self.screen_height = 650
+        self.bg_color = (230,230,230)
---
+++ test.py
@@ -0,0 +1,9 @@
+import sys
+
+pth='C:\\Users\\ASUS\\AppData\\Roaming\\Python\\Python38\\Scripts'
+
+for path in sys.path:
+    print (path)
+
+# #sys.path.append(pth)
+# C:\Users\ASUS\AppData\Roaming\Python\Python38\Scripts
